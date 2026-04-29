f2c0caa82d17c2b4fe7f51f764f35d94e3690947
Rangel <rangel.cmte@gmail.com>
2026-04-29 14:21:16 -0300

    chore: remove deps nao utilizadas

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,7 +4,6 @@
 import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure';
 import { visionTool } from '@sanity/vision';
-import { markdownSchema } from 'sanity-plugin-markdown';
 import { schemaTypes } from './sanity/schemas';
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'placeholder';
@@ -16,6 +15,6 @@ export default defineConfig({
   projectId,
   dataset,
   basePath: '/studio',
-  plugins: [structureTool(), visionTool(), markdownSchema()],
+  plugins: [structureTool(), visionTool()],
   schema: { types: schemaTypes },
 });
